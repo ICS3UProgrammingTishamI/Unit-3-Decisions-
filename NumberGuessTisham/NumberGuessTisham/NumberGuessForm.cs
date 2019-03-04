9771be16f9a05122f2f858fa1729cf9eb6acf7ef
Tisham Islam <45071113+sl1mt1sh@users.noreply.github.com>
2019-03-04 12:34:04 -0500

    Updated to Random Number

--- NumberGuessTisham/NumberGuessTisham/NumberGuessForm.cs
+++ NumberGuessTisham/NumberGuessTisham/NumberGuessForm.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/*
+ * Created by: Tisham
+ * Created on: 1/01/2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #16 - Number Guess/Day #17 - Random Number
+ * This program acquires a number inputted, and checks if it is the numbe rthat I've chosen. 
+ * If it isn't then it sounds a buzzer and tells you the asnwer is wrong, while showing a red x. 
+ * If it is right, it shows a checkmark, makes a noise of sweet victory, and tells you you got the number right
+ * UPDATED: The number you must guess is now randomized
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,37 +18,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace NumberGuessTisham
 {
     public partial class frmNumberGuess : Form
     {
+        //variables and constants
+        const int MIN_VALUE = 1;
+        const int MAX_VALUE = 10;
+        int randomNumber;
+        Random numberGenerator = new Random();
+
         public frmNumberGuess()
         {
             InitializeComponent();
+            //Hides the images and the text because you haven't answered yet
             this.picRightWrong.Hide();
             this.lblRightWrong.Hide();
+            randomNumber = numberGenerator.Next(MIN_VALUE, MAX_VALUE + 1);
         }
-        const int Answer = 6;
-        System.Media.SoundPlayer WrongNoise = new System.Media.SoundPlayer(@"N:\Wrong Answer Sound Effect.wav");
-        System.Media.SoundPlayer CorrectNoise = new System.Media.SoundPlayer(@"N:\Correct Answer Sound Effect.wav");
+       
+
         private void btnGuess_Click(object sender, EventArgs e)
         {
+            //this turns your guess into a number value
             int Guess = int.Parse(txtNumGuess.Text);
-            if (Answer == Guess)
+            //this checks if your answer is right
+            if (Guess == randomNumber)
             {
+                //this shows a checkmark and an affirmation of success if you got it right
                 picRightWrong.Image = Properties.Resources.checkmark;
                 this.picRightWrong.Show();
                 lblRightWrong.Text = "Ding ding, you got it right!";
                 this.lblRightWrong.Show();
+                //this is the code for playing sound, where the files are in the debug folder of this program
+                SoundPlayer CorrectNoise = new SoundPlayer(@"CorrectAnswer.wav");
                 CorrectNoise.Play();
             }
             else
             {
+                //this shows a red x and tells you that you got the answer wrong
                 picRightWrong.Image = Properties.Resources.red_x;
                 this.picRightWrong.Show();
                 lblRightWrong.Text = "Dong dong, you got it wrong";
                 this.lblRightWrong.Show();
+                //this is the code for playing sound, where the files are in the debug folder of this program
+                SoundPlayer WrongNoise = new SoundPlayer(@"WrongBuzzer.wav");
                 WrongNoise.Play();
             }
         }
