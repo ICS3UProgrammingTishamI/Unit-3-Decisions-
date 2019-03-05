4b7c4285bedadec3de1d4ad3fcff599a802a02c3
Tisham Islam <45071113+sl1mt1sh@users.noreply.github.com>
2019-03-05 12:34:40 -0500

    updated rock paper scissors

--- RockPaperScissors/RockPaperScissors/RockPaperScissorsForm.Designer.cs
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RockPaperScissors
 {
-    partial class Form1
+    partial class frmRockPaperScissors
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmRockPaperScissors
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "frmRockPaperScissors";
+            this.Text = "Rock Paper Scissors by Tisham Islam";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
