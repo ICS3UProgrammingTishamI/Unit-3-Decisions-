69ea5b36dea37b3fb6b2f585a0671e397ed46c7d
Tisham Islam <45071113+sl1mt1sh@users.noreply.github.com>
2019-04-02 12:54:39 -0400

    Added nested loops

---
+++ NestedLoopsTishamI/NestedLoopsTishamI/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace NestedLoopsTishamI
+{
+    partial class frmNestedLoops
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListUnicode = new System.Windows.Forms.Button();
+            this.lstUnicode = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnListUnicode
+            // 
+            this.btnListUnicode.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListUnicode.Location = new System.Drawing.Point(236, 39);
+            this.btnListUnicode.Name = "btnListUnicode";
+            this.btnListUnicode.Size = new System.Drawing.Size(221, 90);
+            this.btnListUnicode.TabIndex = 0;
+            this.btnListUnicode.Text = "LIST UNICODE";
+            this.btnListUnicode.UseVisualStyleBackColor = true;
+            this.btnListUnicode.Click += new System.EventHandler(this.btnListUnicode_Click);
+            // 
+            // lstUnicode
+            // 
+            this.lstUnicode.FormattingEnabled = true;
+            this.lstUnicode.Location = new System.Drawing.Point(236, 150);
+            this.lstUnicode.Name = "lstUnicode";
+            this.lstUnicode.Size = new System.Drawing.Size(221, 225);
+            this.lstUnicode.TabIndex = 1;
+            // 
+            // frmNestedLoops
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstUnicode);
+            this.Controls.Add(this.btnListUnicode);
+            this.Name = "frmNestedLoops";
+            this.Text = "Nested Loops by Tisham Islam";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListUnicode;
+        private System.Windows.Forms.ListBox lstUnicode;
+    }
+}
+
---
+++ NestedLoopsTishamI/NestedLoopsTishamI/Form1.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Created by: Tisham Islam
+ * Created on: 02/04/2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #28 - Numbers Nested Loops
+ * This program shows a capital letter, followed by a lowercase letter. 
+ * It then does it again but the lowercase letter 
+ * is moved one place forward in alphabetical order
+ * Once the lowercase letter reaches "z", 
+ * the capital letter shifts forward in alphabetical order
+ * and the lowercase letter goes back to "a"
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NestedLoopsTishamI
+{
+    public partial class frmNestedLoops : Form
+    {
+        public frmNestedLoops()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListUnicode_Click(object sender, EventArgs e)
+        {
+            string character;
+            string character2;
+            int counter;
+            int counter2;
+
+            lstUnicode.Items.Clear();
+
+            for (counter = 65; counter <= 90; counter++)
+            {
+                for (counter2 = 97; counter2 <= 122; counter2++)
+                {
+                    character = Char.ConvertFromUtf32(counter);
+                    character2 = Char.ConvertFromUtf32(counter2);
+                    lstUnicode.Items.Add(character + "->" + character2);
+                }
+            }
+
+
+        }
+    }
+}
